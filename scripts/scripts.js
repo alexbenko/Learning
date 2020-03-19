988ef38213bbde541e2aeb4bd11e74187406ca5a
Alex Benko <benkoalex521@gmail.com>
2020-03-19 15:17:13 -0700

    Deleted un used function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,20 +11,18 @@ var randomPage = function(){
 
 
 
-var test = function(){
-    alert("You Linked Correctly");
-};
+
 
 var quoteOfTheDay = function(){
     
-    alert("Works");
-
     var array =   ["Each path is the right one", "Let everything happen to you,  Beauty and terror Just keep going , No feeling is final"
     ,"It is such a quiet thing to fall... but far more terrible as to admit it."
-    ,"Stuff is just things, it donâ€™t make a fuck"
+    ,"Stuff is just things, it don't make a fuck"
     ,"4","5","6","7","8","9","10","11","12","13","14","15","16","17",
     "Live everyday like it's your last. But wear a condom and pay your bills, just in case it isn't."
-    ,"19"];
+    ,"Its a-me, Mario!"
+    ,
+    "20","21","22","23","24",'25',"26","27","28","29","30","31"];
 
     var date = new Date;
     var day = date.getDate();
@@ -33,13 +31,11 @@ var quoteOfTheDay = function(){
    
     var date = new Date;
     
+   // var finalQuote = document.createTextNode(quote);
+    var id = document.getElementById("quoter");
+    id.innerHTML = quote;
+    //document.body.appendChild(finalQuote);
 
-  //  document.getElementById("quoter").textContent=quotesObj.quote;
     
-    
-    var finalQuote = document.createTextNode(quote);
-
-    document.body.appendChild(finalQuote);
-
 
 }
