47d2a6559e80b1b4cc1edee2b93e62e238582e78
Alex Benko <benkoalex521@gmail.com>
2020-03-19 01:06:11 -0700

    Fixed the quote of the day generator so it acurately added a quote to the page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,32 @@ var randomPage = function(){
 var test = function(){
     alert("You Linked Correctly");
 };
+
+var quoteOfTheDay = function(){
+    
+    alert("Works");
+
+    var array =   ["Each path is the right one", "Let everything happen to you,  Beauty and terror Just keep going , No feeling is final"
+    ,"It is such a quiet thing to fall... but far more terrible as to admit it."
+    ,"Stuff is just things, it donâ€™t make a fuck"
+    ,"4","5","6","7","8","9","10","11","12","13","14","15","16","17",
+    "Live everyday like it's your last. But wear a condom and pay your bills, just in case it isn't."
+    ,"19"];
+
+    var date = new Date;
+    var day = date.getDate();
+
+    var quote = array[day];
+   
+    var date = new Date;
+    
+
+  //  document.getElementById("quoter").textContent=quotesObj.quote;
+    
+    
+    var finalQuote = document.createTextNode(quote);
+
+    document.body.appendChild(finalQuote);
+
+
+}
